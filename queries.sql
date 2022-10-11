e15ed2fdc09f846ccc00e8b406447e816672096c
Oanh Huynh <oanhhuynh01@gmail.com>
2022-10-10 23:27:43 -0400

    Initial commit

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 -- Part 1: Test it with SQL
-
+/* employer varchar(25)
+    id int
+    name varchar (255)
+    skills varchar(255)
+    */
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
